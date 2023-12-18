a9363a2234d9f7256e4e979ebc13bc0f9b4117f8
KhoaTheLazyCat <khoathelazycat@gmail.com>
2023-12-18 16:09:54 +0700

    initial commit

---
+++ lua/plugins.lua
@@ -0,0 +1,21 @@
+local status, packer = pcall(require, 'packer')
+if (not status) then
+  print('Packer is not installed')
+  return
+end
+
+vim.cmd [[packadd packer.nvim]]
+
+packer.startup(function (use)
+  use 'wbthomason/packer.nvim'
+  use {
+    'svrana/neosolarized.nvim',
+    requires = { 'tjdevries/colorbuddy.nvim' }
+  }
+  use 'onsails/lspkind-nvim'
+  use 'L3MON4D3/LuaSnip'
+  use 'hrsh7th/cmp-buffer'
+  use 'hrsh7th/cmp-nvim-lsp'
+  use 'hrsh7th/nvim-cmp'
+  use 'hoob3rt/lualine.nvim'
+end)
